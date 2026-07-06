8a7302d46e7b3d40d7958a3a4cbee9c328438b20
alanua <alanua@users.noreply.github.com>
2026-07-06 22:05:08 +0200

    Add package initializer

---
+++ src/dios/__init__.py
@@ -0,0 +1,5 @@
+"""DIOS core data models."""
+
+from .models import DataClass, QuantityRecord, SourceRef, VerificationStatus
+
+__all__ = ["DataClass", "QuantityRecord", "SourceRef", "VerificationStatus"]
